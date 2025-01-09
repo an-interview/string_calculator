93713b27498187deebba62f2c08a2eb3c72948b6
abhijit navale <abhijitnwork@gmail.com>
2025-01-09 11:54:02 +0530

    test for single number with dynamic separatorc

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -32,5 +32,9 @@ RSpec.describe StringCalculator do
     it 'return 15 for //;1,2\n3;4;5' do
       expect(StringCalculator.calculate('//;1,2\n3;4;5')).to eq(15)
     end
+
+    it 'return 1 for //;1' do
+      expect(StringCalculator.calculate('//;1')).to eq(1)
+    end
   end
 end
